56bddeeb8692908ee1a6f45808d6a69478859480
Steven <alu0101619613@ull.edu.es>
2025-02-14 14:47:48 +0000

    updated

--- PRINCIPIO_DE_COMPUTADORES/sesiones_academicas/S3/pruebas/prueba_impresion_entero.s
+++ PRINCIPIO_DE_COMPUTADORES/sesiones_academicas/S3/pruebas/prueba_impresion_entero.s
@@ -1,9 +1,16 @@
         .text
 main: 
-        li $v0, 4
-        move $s0, $v0
+        li $v0, 5
         syscall
+        move $s0, $v0
 
         li $v0, 1
         move $a0, $s0
         syscall
+
+        li $v0, 10
+        syscall
+
+        .data
+strTitulo: .asciiz "Prueba de impresión de número enteros\n"
+strAutor: .asciiz "Steven, 70652456K, alu1619613@ull.edu.es\n" 
