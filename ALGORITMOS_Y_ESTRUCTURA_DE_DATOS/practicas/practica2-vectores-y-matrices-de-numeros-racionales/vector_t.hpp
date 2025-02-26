63d4b04c92a02b8ec393fac36c0bae15c20ebea7
Steven <alu0101619613@ull.edu.es>
2025-02-26 14:05:44 +0000

    updated

---
+++ ALGORITMOS_Y_ESTRUCTURA_DE_DATOS/practicas/practica2-vectores-y-matrices-de-numeros-racionales/vector_t.hpp
@@ -0,0 +1,223 @@
+// AUTOR: 
+// FECHA: 
+// EMAIL: 
+// VERSION: 1.0
+// ASIGNATURA: Algoritmos y Estructuras de Datos
+// PRÁCTICA Nº: 2
+// COMENTARIOS: se indican entre [] las pautas de estilo aplicadas de
+//              "C++ Programming Style Guidelines"
+//              https://geosoft.no/development/cppstyle.html
+
+#pragma once
+
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+
+template<class T>
+class vector_t
+{
+public:
+// Constructor por defecto de la clase vector_t
+  vector_t(const int = 0);
+// Destructor de la clase vector_t 
+  ~vector_t();
+// Método para la longitud del vector
+  void resize(const int);
+  
+  // getters
+  T get_val(const int) const;
+  int get_size(void) const;
+  
+  // setters
+  void set_val(const int, const T);
+  
+  // getters-setters
+  T& at(const int);
+  T& operator[](const int);
+  
+  // getters constantes
+  const T& at(const int) const;
+  const T& operator[](const int) const;
+
+  // Métodos de escritura y lectura de la clase
+  void write(ostream& = cout) const;
+  void read(istream& = cin);
+
+private:
+    // Atributos de la clase 
+  T *v_;
+  int sz_;
+  
+  // Método que contruye 
+  void build(void);
+  // Método que destruye
+  void destroy(void);
+};
+
+
+
+template<class T>
+vector_t<T>::vector_t(const int n)
+{ sz_ = n;
+  build();
+}
+
+
+template<class T>
+vector_t<T>::~vector_t()
+{
+  destroy();
+}
+
+
+
+template<class T>
+void
+vector_t<T>::build()
+{
+  v_ = NULL;
+  if (sz_ != 0) {
+    v_ = new T[sz_];
+    assert(v_ != NULL);
+  }
+}
+
+
+template<class T>
+void
+vector_t<T>::destroy()
+{
+  if (v_ != NULL) {
+    delete[] v_;
+    v_ = NULL;
+  }
+  sz_ = 0;
+}
+
+
+template<class T>
+void
+vector_t<T>::resize(const int n)
+{
+  destroy();
+  sz_ = n;
+  build();
+}
+
+
+template<class T>
+inline T
+vector_t<T>::get_val(const int i) const
+{
+  assert(i >= 0 && i < get_size());
+  return v_[i];
+}
+
+
+template<class T>
+inline int
+vector_t<T>::get_size() const
+{
+  return sz_;
+}
+
+
+
+template<class T>
+void
+vector_t<T>::set_val(const int i, const T d)
+{
+  assert(i >= 0 && i < get_size());
+  v_[i] = d;
+}
+
+
+
+template<class T>
+T&
+vector_t<T>::at(const int i)
+{
+  assert(i >= 0 && i < get_size());
+  return v_[i];
+}
+
+
+
+template<class T>
+T&
+vector_t<T>::operator[](const int i)
+{
+  return at(i);
+}
+
+
+
+template<class T>
+const T&
+vector_t<T>::at(const int i) const
+{
+  assert(i >= 0 && i < get_size());
+  return v_[i];
+}
+
+
+
+template<class T>
+const T&
+vector_t<T>::operator[](const int i) const
+{
+  return at(i);
+}
+
+
+
+template<class T>
+void
+vector_t<T>::write(ostream& os) const
+{ 
+  os << get_size() << ":\t";
+  for (int i = 0; i < get_size(); i++)
+    os << at(i) << "\t";
+  os << endl;
+}
+
+
+
+template<class T>
+void
+vector_t<T>::read(istream& is)
+{
+  is >> sz_;
+  resize(sz_);
+  for (int i = 0; i < sz_; ++i)
+    is >> at(i);
+}
+
+
+// FASE II: producto escalar
+template<class T>
+T
+scal_prod(const vector_t<T>& v, const vector_t<T>& w)
+{
+  double producto_escalar{0.0};
+  for(int i = 0; i < v.get_size(); i++) {
+    producto_escalar = producto_escalar + v[i] * w[i];
+  }
+  return producto_escalar; 
+}
+
+
+
+double
+scal_prod(const vector_t<rational_t>& v, const vector_t<rational_t>& w)
+{
+  double producto_escalar{0.0};
+  for(int i = 0; i < v.get_size(); i++) {
+    rational_t total{0,1};
+    total = v[i] * w[i];
+    producto_escalar = producto_escalar + total.value();
+  }
+  return producto_escalar; 
+}
